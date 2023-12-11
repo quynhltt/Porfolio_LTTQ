ab52d1414b62783b05a80b627054891fca1ae2fd
Le Thanh Truc Quynh <quynhltt2@tpb.com.vn>
2023-12-11 17:39:43 +0700

    commit code

--- src/pages/ProjectPage/ProjectPage.jsx
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { dataProject } from "../../assets/data/dataProject";
 import Intro from "./components/Intro/Intro";
+import Content from "./components/Content/Content";
 
 export default function ProjectPage() {
   const [data, setData] = useState([]);
@@ -12,6 +13,7 @@ export default function ProjectPage() {
   return (
     <div className="project-page">
       <Intro data={data} />
+      <Content data={data} />
     </div>
   );
 }
--- src/pages/ProjectPage/components/Content/Content.jsx
+++ src/pages/ProjectPage/components/Content/Content.jsx
@@ -1,5 +1,37 @@
 import React from "react";
 
-export default function Content() {
-  return <div>Content</div>;
+export default function Content({ data }) {
+  let { desc, size, role } = data;
+  let renderProjectDetail = () => {
+    return desc.map((item) => {
+      return (
+        <li>
+          <p className="text-lg">{item}</p>
+        </li>
+      );
+    });
+  };
+  return (
+    <section className="content">
+      <div className="container w-2/3 mx-auto">
+        <div className="carousel"></div>
+        <div className="overview space-y-5">
+          <div>
+            <h3 className="tracking-wider text-2xl font-medium">Description</h3>
+            <ul className="mt-2 space-y-1"></ul>
+          </div>
+          <div className="flex items-center space-x-2">
+            <h3 className="tracking-wider text-2xl font-medium">Team size: </h3>
+            <p className="text-lg">{size}</p>
+          </div>
+          <div className="flex items-center space-x-2">
+            <h3 className="tracking-wider text-2xl font-medium">Role: </h3>
+            <p className="text-lg">{role}</p>
+          </div>
+        </div>
+        <div className="technologies"></div>
+        <div className="link"></div>
+      </div>
+    </section>
+  );
 }
